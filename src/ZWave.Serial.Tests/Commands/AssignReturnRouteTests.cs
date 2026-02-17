ef3fdac2bfa2be4c456d8dab0fc00292edde53b4
David Federman <dfederm@microsoft.com>
2026-02-17 03:43:45 -0800

    Move Serial API to its own project

---
+++ src/ZWave.Serial.Tests/Commands/AssignReturnRouteTests.cs
@@ -0,0 +1,41 @@
+﻿using ZWave.Serial;
+using ZWave.Serial.Commands;
+
+namespace ZWave.Serial.Tests.Commands;
+
+[TestClass]
+public class AssignReturnRouteTests : CommandTestBase
+{
+    private record AssignReturnRouteCallbackData(byte SessionId, TransmissionStatus Status);
+
+    [TestMethod]
+    public void Request()
+        => TestSendableCommand(
+            DataFrameType.REQ,
+            CommandId.AssignReturnRoute,
+            new[]
+            {
+                (
+                    Request: AssignReturnRouteRequest.Create(
+                        sourceNodeId: 2,
+                        destinationNodeId: 1,
+                        sessionId: 3),
+                    ExpectedCommandParameters: new byte[] { 0x02, 0x01, 0x03 }
+                ),
+            });
+
+    [TestMethod]
+    public void Callback()
+        => TestReceivableCommand<AssignReturnRouteCallback, AssignReturnRouteCallbackData>(
+            DataFrameType.REQ,
+            CommandId.AssignReturnRoute,
+            new[]
+            {
+                (
+                    CommandParameters: new byte[] { 0x03, 0x00 },
+                    ExpectedData: new AssignReturnRouteCallbackData(
+                        SessionId: 3,
+                        Status: TransmissionStatus.Ok)
+                )
+            });
+}
---
+++ src/ZWave.Serial.Tests/Commands/SendDataBridgeTests.cs
@@ -0,0 +1,47 @@
+﻿using ZWave.Serial;
+using ZWave.Serial.Commands;
+
+namespace ZWave.Serial.Tests.Commands;
+
+[TestClass]
+public class SendDataBridgeTests : CommandTestBase
+{
+    private record SendDataBridgeCallbackData(
+        byte SessionId,
+        TransmissionStatus TransmissionStatus,
+        TransmissionStatusReport? TransmissionStatusReport);
+
+    [TestMethod]
+    public void Request()
+        => TestSendableCommand(
+            DataFrameType.REQ,
+            CommandId.SendDataBridge,
+            new[]
+            {
+                (
+                    Request: SendDataBridgeRequest.Create(
+                        sourceNodeId: 1,
+                        destinationNodeId: 2,
+                        data: new byte[] { 0x25, 0x01 },
+                        txOptions: TransmissionOptions.ACK,
+                        sessionId: 3),
+                    ExpectedCommandParameters: new byte[] { 0x01, 0x02, 0x02, 0x25, 0x01, 0x01, 0x03 }
+                ),
+            });
+
+    [TestMethod]
+    public void Callback()
+        => TestReceivableCommand<SendDataBridgeCallback, SendDataBridgeCallbackData>(
+            DataFrameType.REQ,
+            CommandId.SendDataBridge,
+            new[]
+            {
+                (
+                    CommandParameters: new byte[] { 0x03, 0x00 },
+                    ExpectedData: new SendDataBridgeCallbackData(
+                        SessionId: 3,
+                        TransmissionStatus: TransmissionStatus.Ok,
+                        TransmissionStatusReport: null)
+                )
+            });
+}
